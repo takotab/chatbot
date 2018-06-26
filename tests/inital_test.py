c54fc55d630fd71637b06ee5cd8545174fecb940
tako_desktop <takotabak@gmail.com>
2018-06-26 14:03:34 +0200

    deployed to gcp

--- tests/inital_test.py
+++ tests/inital_test.py
@@ -4,7 +4,7 @@ import pytest
 import requests
 
 # @pytest.mark.usefixtures('app')
-def start_test(id_ = '25468eee'):
+def test_start(id_ = '25468eee'):
     """
     GIVEN a Flask application
     WHEN the '/login' page is posted to (POST)
@@ -13,7 +13,7 @@ def start_test(id_ = '25468eee'):
     response_content = interact(id_, 'e')
 
     assert "Waterstand doorgeven" in response_content
-    assert "Waterstand analyse" in response_content
+    assert "Watergebruik analyse" in response_content
     assert "Factuur inzien" in response_content
     assert "Adreswijziging" in response_content
 
@@ -22,14 +22,17 @@ def interact(id_, msg):
     url = 'http://' + 'localhost:8080'
     response = requests.get(url + '/get',params={"msg":msg + id_})
     assert response.status_code == 200
-    return response.content
+    return str(response.content)
 
-def waterstand_doorgeven_test():
+def test_waterstand_doorgeven():
     N = 4
     id_ = ''.join(random.choice(string.ascii_uppercase + string.digits)
                       for _ in range(N))
-    start_test(id_ = id_)
+    test_start(id_ = id_)
     response_content = interact(id_, "waterstand doorgeven")
+
+    assert "Wat is" in response_content
+    assert "naam" in response_content
 
 
     

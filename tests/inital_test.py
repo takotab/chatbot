2d4e607439034e64b637c5be5e5ac578162bff0c
tako_desktop <takotabak@gmail.com>
2018-06-26 13:54:04 +0200

    testing on the app!! yes!

--- tests/inital_test.py
+++ tests/inital_test.py
@@ -1,28 +1,35 @@
+import random
+import string
 import pytest
+import requests
 
-@pytest.mark.usefixtures('app')
-def test_watermeter_story(app):
+# @pytest.mark.usefixtures('app')
+def start_test(id_ = '25468eee'):
     """
     GIVEN a Flask application
     WHEN the '/login' page is posted to (POST)
     THEN check the response is valid
     """
-    id = '__id__' + '938490abve'
-    response = app.post('/get',
-                                data=dict(msg='e' + id),
-                                follow_redirects=True)
+    response_content = interact(id_, 'e')
+
+    assert "Waterstand doorgeven" in response_content
+    assert "Waterstand analyse" in response_content
+    assert "Factuur inzien" in response_content
+    assert "Adreswijziging" in response_content
+
+def interact(id_, msg):
+    id_ = '__id__' + id_
+    url = 'http://' + 'localhost:8080'
+    response = requests.get(url + '/get',params={"msg":msg + id_})
     assert response.status_code == 200
-    print(response)
-    assert b"Thanks for logging in, patkennedy79@gmail.com!" in response.data
-    assert b"Welcome patkennedy79@gmail.com!" in response.data
-    assert b"Flask User Management" in response.data
-    assert b"Logout" in response.data
-    assert b"Login" not in response.data
-    assert b"Register" not in response.data
- 
-    """
-    GIVEN a Flask application
-    WHEN the '/logout' page is requested (GET)
-    THEN check the response is valid
-    """
-    # response = app.get('/logout', follow_redirects=True)
+    return response.content
+
+def waterstand_doorgeven_test():
+    N = 4
+    id_ = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                      for _ in range(N))
+    start_test(id_ = id_)
+    response_content = interact(id_, "waterstand doorgeven")
+
+
+    

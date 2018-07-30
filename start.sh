07fd8b92843ed268cf044f95fb62418be6a285ad
tako_viao <takotabak@gmail.com>
2018-07-30 08:55:13 +0200

    add sudo again to start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 sudo docker kill $(sudo docker ps -q)
-docker build -t chatbot .
+sudo docker build -t chatbot .
 echo "docker build"
-docker run -it -p 8080:8080 chatbot
+sudo docker run -it -p 8080:8080 chatbot
 
 #echo "start tests"
 #python3 -m pytest tests
